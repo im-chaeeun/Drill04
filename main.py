680ab951bc51ec1e22271e08ce926a69d687d56f
im-chaeeun <ce0118@tukorea.ac.kr>
2023-09-28 16:08:28 +0900

    캐릭터의 좌우이동 완료

--- main.py
+++ main.py
@@ -6,30 +6,39 @@ character=load_image('character_sheet.png')
 TUK_WIDTH, TUK_HEIGHT = 1280, 1024
 tuk_ground=load_image('TUK_GROUND.png')
 
-running=True
-
 def handle_events():
-    global running
+    global running, x, dir
 
     events=get_events()
     for event in events:
         if event.type==SDL_QUIT:
             running=False
-        elif event.type==SDL_KEYDOWN and event.key==SDLK_ESCAPE:
-            running=False
+        elif event.type==SDL_KEYDOWN:
+            if event.key==SDLK_RIGHT:
+                dir+=1
+            elif event.key == SDLK_LEFT:
+                dir-=1
+            elif event.key==SDLK_ESCAPE:
+                running=False
+        elif event.type == SDL_KEYUP:
+            if event.key==SDLK_RIGHT:
+                dir-=1
+            elif event.key==SDLK_LEFT:
+                dir+=1
 
+running=True
+x=800//2
 frame=0
-for x in range(0, 850, 10):
+dir=0
+
+while running:
     clear_canvas()
-    tuk_ground.draw(TUK_WIDTH//2, TUK_HEIGHT//2)
-    character.clip_draw(frame*85, 315, 85, 68, x, 90)
+    tuk_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
+    character.clip_draw(frame * 85, 315, 85, 68, x, 90)
     update_canvas()
-
     handle_events()
-    if not running:
-        break
-
-    frame=(frame+1)%10
+    frame = (frame + 1) % 10
+    x+=dir*5
     delay(0.05)
 
 close_canvas()
