707fc39f51540d62b4f3ce0d6c484e2cace07e39
im-chaeeun <ce0118@tukorea.ac.kr>
2023-09-28 15:48:07 +0900

    캐릭터 오른쪽으로 이동 완료

--- main.py
+++ main.py
@@ -6,5 +6,13 @@ character=load_image('character_sheet.png')
 TUK_WIDTH, TUK_HEIGHT = 1280, 1024
 tuk_ground=load_image('TUK_GROUND.png')
 
-close_canvas()
+frame=0
+for x in range(0, 850, 10):
+    clear_canvas()
+    tuk_ground.draw(TUK_WIDTH//2, TUK_HEIGHT//2)
+    character.clip_draw(frame*85, 315, 85, 68, x, 90)
+    update_canvas()
+    frame=(frame+1)%10
+    delay(0.05)
 
+close_canvas()
