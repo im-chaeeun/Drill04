f0fdc3b7e61488fe8800f376bdd7824e93fb387d
im-chaeeun <ce0118@tukorea.ac.kr>
2023-09-28 15:52:45 +0900

    esc를 이용한 종료 완성

--- main.py
+++ main.py
@@ -6,12 +6,29 @@ character=load_image('character_sheet.png')
 TUK_WIDTH, TUK_HEIGHT = 1280, 1024
 tuk_ground=load_image('TUK_GROUND.png')
 
+running=True
+
+def handle_events():
+    global running
+
+    events=get_events()
+    for event in events:
+        if event.type==SDL_QUIT:
+            running=False
+        elif event.type==SDL_KEYDOWN and event.key==SDLK_ESCAPE:
+            running=False
+
 frame=0
 for x in range(0, 850, 10):
     clear_canvas()
     tuk_ground.draw(TUK_WIDTH//2, TUK_HEIGHT//2)
     character.clip_draw(frame*85, 315, 85, 68, x, 90)
     update_canvas()
+
+    handle_events()
+    if not running:
+        break
+
     frame=(frame+1)%10
     delay(0.05)
 
